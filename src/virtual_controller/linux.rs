e4858fb7855537115afc72c2d6b81e606777a313
Lennard Kittner <lennard@kittner.dev>
2026-06-06 01:50:39 +0200

    Add analog controller inputs and refactor virtual controller for analog inputs

--- src/virtual_controller/linux.rs
+++ src/virtual_controller/linux.rs
@@ -1,4 +1,9 @@
+use std::i32;
+use std::time::Duration;
+
 use crate::virtual_controller::{AbstractVirtualController, ControllerInput};
+use uinput::event::absolute::Position;
+use uinput::event::Controller;
 use uinput::{event::controller, Device, Result};
 
 /// Xbox series x
@@ -9,32 +14,45 @@ const VENDOR_ID: u16 = 0x045e;
 const NAME: &str = "Xbox Controller";
 
 /// map generic controller input the the uinput specific events
-fn map_controller_input(input: ControllerInput) -> uinput::event::Controller {
-    use uinput::event::Controller;
+fn map_digital_controller_input(input: ControllerInput) -> (uinput::event::Controller, bool) {
     match input {
-        ControllerInput::South => Controller::GamePad(controller::GamePad::South),
-        ControllerInput::A => Controller::GamePad(controller::GamePad::A),
-        ControllerInput::East => Controller::GamePad(controller::GamePad::East),
-        ControllerInput::B => Controller::GamePad(controller::GamePad::B),
-        ControllerInput::C => Controller::GamePad(controller::GamePad::C),
-        ControllerInput::North => Controller::GamePad(controller::GamePad::North),
-        ControllerInput::X => Controller::GamePad(controller::GamePad::X),
-        ControllerInput::West => Controller::GamePad(controller::GamePad::West),
-        ControllerInput::Y => Controller::GamePad(controller::GamePad::Y),
-        ControllerInput::Z => Controller::GamePad(controller::GamePad::Z),
-        ControllerInput::LeftTrigger => Controller::GamePad(controller::GamePad::TL),
-        ControllerInput::RightTrigger => Controller::GamePad(controller::GamePad::TR),
-        ControllerInput::LeftBumper => Controller::GamePad(controller::GamePad::TL2),
-        ControllerInput::RightBumper => Controller::GamePad(controller::GamePad::TR2),
-        ControllerInput::Select => Controller::GamePad(controller::GamePad::Select),
-        ControllerInput::Start => Controller::GamePad(controller::GamePad::Start),
-        ControllerInput::Mode => Controller::GamePad(controller::GamePad::Mode),
-        ControllerInput::LeftThumb => Controller::GamePad(controller::GamePad::ThumbL),
-        ControllerInput::RightThumb => Controller::GamePad(controller::GamePad::ThumbR),
-        ControllerInput::Up => Controller::DPad(controller::DPad::Up),
-        ControllerInput::Down => Controller::DPad(controller::DPad::Down),
-        ControllerInput::Left => Controller::DPad(controller::DPad::Left),
-        ControllerInput::Right => Controller::DPad(controller::DPad::Right),
+        ControllerInput::South(pressed) => {
+            (Controller::GamePad(controller::GamePad::South), pressed)
+        }
+        ControllerInput::A(pressed) => (Controller::GamePad(controller::GamePad::A), pressed),
+        ControllerInput::East(pressed) => (Controller::GamePad(controller::GamePad::East), pressed),
+        ControllerInput::B(pressed) => (Controller::GamePad(controller::GamePad::B), pressed),
+        ControllerInput::C(pressed) => (Controller::GamePad(controller::GamePad::C), pressed),
+        ControllerInput::North(pressed) => {
+            (Controller::GamePad(controller::GamePad::North), pressed)
+        }
+        ControllerInput::X(pressed) => (Controller::GamePad(controller::GamePad::X), pressed),
+        ControllerInput::West(pressed) => (Controller::GamePad(controller::GamePad::West), pressed),
+        ControllerInput::Y(pressed) => (Controller::GamePad(controller::GamePad::Y), pressed),
+        ControllerInput::Z(pressed) => (Controller::GamePad(controller::GamePad::Z), pressed),
+        ControllerInput::LeftBumper(pressed) => {
+            (Controller::GamePad(controller::GamePad::TL2), pressed)
+        }
+        ControllerInput::RightBumper(pressed) => {
+            (Controller::GamePad(controller::GamePad::TR2), pressed)
+        }
+        ControllerInput::Select(pressed) => {
+            (Controller::GamePad(controller::GamePad::Select), pressed)
+        }
+        ControllerInput::Start(pressed) => {
+            (Controller::GamePad(controller::GamePad::Start), pressed)
+        }
+        ControllerInput::Mode(pressed) => (Controller::GamePad(controller::GamePad::Mode), pressed),
+        ControllerInput::LeftThumb(pressed) => {
+            (Controller::GamePad(controller::GamePad::ThumbL), pressed)
+        }
+        ControllerInput::RightThumb(pressed) => {
+            (Controller::GamePad(controller::GamePad::ThumbR), pressed)
+        }
+        ControllerInput::Up(pressed) => (Controller::DPad(controller::DPad::Up), pressed),
+        ControllerInput::Down(pressed) => (Controller::DPad(controller::DPad::Down), pressed),
+        ControllerInput::Left(pressed) => (Controller::DPad(controller::DPad::Left), pressed),
+        ControllerInput::Right(pressed) => (Controller::DPad(controller::DPad::Right), pressed),
         _ => unimplemented!(),
     }
 }
@@ -44,6 +62,9 @@ pub struct VirtualController {
     device: Device,
 }
 
+const STICK_MIN: i32 = -32768;
+const STICK_MAX: i32 = 32767;
+
 impl VirtualController {
     /// create new virtual controller
     pub fn new() -> Result<VirtualController> {
@@ -51,26 +72,94 @@ impl VirtualController {
             .name(NAME)?
             .vendor(VENDOR_ID)
             .product(PRODUCT_ID)
-            .event(uinput::event::Controller::All)?
+            .event(uinput::event::Controller::GamePad(controller::GamePad::A))?
+            .event(uinput::event::Controller::GamePad(controller::GamePad::B))?
+            .event(uinput::event::Controller::GamePad(controller::GamePad::X))?
+            .event(uinput::event::Controller::GamePad(controller::GamePad::Y))?
+            .event(uinput::event::Controller::GamePad(controller::GamePad::TL))?
+            .event(uinput::event::Controller::GamePad(controller::GamePad::TR))?
+            .event(uinput::event::Controller::GamePad(
+                controller::GamePad::Select,
+            ))?
+            .event(uinput::event::Controller::GamePad(
+                controller::GamePad::Start,
+            ))?
+            .event(uinput::event::Controller::GamePad(
+                controller::GamePad::ThumbL,
+            ))?
+            .event(uinput::event::Controller::GamePad(
+                controller::GamePad::ThumbR,
+            ))?
+            // Left stick
+            .event(uinput::event::absolute::Absolute::Position(Position::X))?
+            .min(STICK_MIN)
+            .max(STICK_MAX)
+            .fuzz(0)
+            .flat(0)
+            .event(uinput::event::absolute::Absolute::Position(Position::Y))?
+            .min(STICK_MIN)
+            .max(STICK_MAX)
+            .fuzz(0)
+            .flat(0)
+            // Right stick
+            .event(uinput::event::absolute::Absolute::Position(Position::RX))?
+            .min(STICK_MIN)
+            .max(STICK_MAX)
+            .fuzz(0)
+            .flat(0)
+            .event(uinput::event::absolute::Absolute::Position(Position::RY))?
+            .min(STICK_MIN)
+            .max(STICK_MAX)
+            .fuzz(0)
+            .flat(0)
+            // Triggers
+            .event(uinput::event::absolute::Absolute::Position(Position::Z))?
+            .min(0)
+            .max(255)
+            .fuzz(0)
+            .flat(0)
+            .event(uinput::event::absolute::Absolute::Position(Position::RZ))?
+            .min(0)
+            .max(255)
+            .fuzz(0)
+            .flat(0)
             .create()?;
         Ok(Self { device })
+    }
+
+    /// helper for digital inputs
+    fn perform_digital_input(&mut self, input: ControllerInput) -> anyhow::Result<()> {
+        let (input, pressed) = map_digital_controller_input(input);
+        if pressed {
+            self.device.press(&input)?;
+        } else {
+            self.device.release(&input)?;
+        }
+        Ok(())
     }
 }
 
 impl AbstractVirtualController for VirtualController {
-    fn press_button(&mut self, input: ControllerInput) -> anyhow::Result<()> {
-        self.device.press(&map_controller_input(input))?;
+    fn send_input(&mut self, input: ControllerInput) -> anyhow::Result<()> {
+        match input {
+            ControllerInput::RightJoyStick(x, y) => {
+                todo!();
+                self.device.position(
+                    &uinput::event::absolute::Position::X,
+                    ((STICK_MAX - STICK_MIN) as f32 * x) as i32,
+                )?;
+                self.device.position(
+                    &uinput::event::absolute::Position::Y,
+                    ((STICK_MAX - STICK_MIN) as f32 * y) as i32,
+                )?;
+                self.device.synchronize()?;
+            }
+            ControllerInput::LeftJoyStick(x, y) => todo!(),
+            ControllerInput::LeftTrigger(force) => todo!(),
+            ControllerInput::RightTrigger(force) => todo!(),
+            digital_input => self.perform_digital_input(digital_input)?,
+        }
         self.device.synchronize()?;
         Ok(())
-    }
-
-    fn release_button(&mut self, input: ControllerInput) -> anyhow::Result<()> {
-        self.device.release(&map_controller_input(input))?;
-        self.device.synchronize()?;
-        Ok(())
-    }
-
-    fn joystick(&mut self, input: ControllerInput) -> anyhow::Result<()> {
-        todo!()
     }
 }
--- src/virtual_controller/mod.rs
+++ src/virtual_controller/mod.rs
@@ -4,42 +4,40 @@ pub mod linux;
 #[allow(unused_imports)]
 pub use linux::VirtualController;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 /// Backed independent controller inputs
 pub enum ControllerInput {
-    South,
-    A,
-    East,
-    B,
-    C,
-    North,
-    X,
-    West,
-    Y,
-    Z,
-    LeftBumper,
-    RightBumper,
-    LeftTrigger,
-    RightTrigger,
-    Select,
-    Start,
-    Mode,
-    LeftThumb,
-    RightThumb,
-    Up,
-    Down,
-    Left,
-    Right,
-    Menu,
-    Home,
+    South(bool),
+    A(bool),
+    East(bool),
+    B(bool),
+    C(bool),
+    North(bool),
+    X(bool),
+    West(bool),
+    Y(bool),
+    Z(bool),
+    LeftBumper(bool),
+    RightBumper(bool),
+    Select(bool),
+    Start(bool),
+    Mode(bool),
+    LeftThumb(bool),
+    RightThumb(bool),
+    Up(bool),
+    Down(bool),
+    Left(bool),
+    Right(bool),
+    Menu(bool),
+    Home(bool),
+    RightJoyStick(f32, f32),
+    LeftJoyStick(f32, f32),
+    LeftTrigger(f32),
+    RightTrigger(f32),
 }
 
 /// Used to implement OS independent virtual controller
 pub trait AbstractVirtualController {
-    /// simulate a button press
-    fn press_button(&mut self, input: ControllerInput) -> anyhow::Result<()>;
-    /// simulate a button release
-    fn release_button(&mut self, input: ControllerInput) -> anyhow::Result<()>;
-    /// simulate joy stick movement
-    fn joystick(&mut self, input: ControllerInput) -> anyhow::Result<()>;
+    /// Send a virtual controller input
+    fn send_input(&mut self, input: ControllerInput) -> anyhow::Result<()>;
 }
